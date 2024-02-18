c5033c30f3b6f5c27e3e803bf4a33cff7ad7ecf9
Dacounter95 <146789960+germanhytsystem@users.noreply.github.com>
2024-02-18 00:48:06 -0500

    feat: Avance front - Consumo Apis -Flask

---
+++ appfrontendpythontesis/main.py
@@ -0,0 +1,30 @@
+from flask import Flask, render_template, request
+import requests
+
+app = Flask(__name__)
+
+
+@app.route('/', methods=['GET', 'POST'])
+def home():
+    # return 'Hola Mundo'
+    if request.method == 'GET':
+        url = "http://127.0.0.1:8000/Peliculas"
+        peliculas = requests.get(url)
+        peliculasData = peliculas.json()
+        print(peliculasData)
+        return render_template("index.html", data=peliculasData)
+
+    if request.form['search']:
+        url = "http://127.0.0.1:8000/Peliculas?id="+request.form['search']
+        peliculas = requests.get(url)
+        peliculasData = peliculas.json()
+        return render_template("index.html", data=peliculasData['data'])
+
+    if request.form['peliculaid']:
+        return render_template("paginatest.html")
+    else:
+        return render_template("index.html")
+
+
+if __name__ == "__name__":
+    app.run(debug=True, port=5200)
