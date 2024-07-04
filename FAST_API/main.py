3cfb46dbc765a36d47b15d9ca28143ca8b02d164
germanhytsystem <germanhuaytalla22@gmail.com>
2024-07-04 01:58:21 -0500

    fix: resolve error CORS

--- FAST_API/main.py
+++ FAST_API/main.py
@@ -1,9 +1,17 @@
 from fastapi import FastAPI
 from typing import Union
 from router import Peliculas, Login, Ratings
-
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(Peliculas.router)
 app.include_router(Login.router)
